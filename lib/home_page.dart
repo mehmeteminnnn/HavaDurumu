0e02a64e34e960ad02e974839f1c971697e4533b
Mehmet Emin Tok <mhmtmntok@gmail.com>
2023-12-16 16:40:47 +0300

    http paketi yardımıyla Openweather APİ'dan veri çeken getsehir fonksiyonu yazıldı.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hava_durumu/search_page.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,6 +10,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String sehirAdi = 'Ankara';
+  double sicaklik = 43.0;
+  String key = "b341c37285f7a448e34f199b78d963bc";
+
+  void getSehir() {
+    Future<http.Response> fetchAlbum() {
+      return http.get(Uri.parse(
+          'https://api.openweathermap.org/data/2.5/weather?q=$sehirAdi&appid=$sicaklik'));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -24,14 +36,14 @@ class _HomePageState extends State<HomePage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text("20°C",
+                Text("$sicaklik°C",
                     style:
                         TextStyle(fontSize: 70, fontWeight: FontWeight.bold)),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
-                      "MANİSA",
+                      sehirAdi,
                       style:
                           TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                     ),
